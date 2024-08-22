43ab047ae6ab6aeac689ae9ed67156ab73b1ada5
Muhammad-Moiz-Shafiq <m.moiz.cse@gmail.com>
2024-08-22 22:57:42 +0500

    added keyboard input detection and also hosted the app

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:async';
 
 import 'HighScoreTile.dart';
@@ -25,8 +26,15 @@ class _MainScreenState extends State<MainScreen> {
   var currentDirection = SnakeDirection.DOWN;
   bool isStarted = false;
   final _nameController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
   List<String> docsID = [];
   //late final Future getHSDocsIDs;
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
   @override
   void initState() {
     //getHSDocsIDs = getDocID();
@@ -162,118 +170,136 @@ class _MainScreenState extends State<MainScreen> {
     double screenWidth = MediaQuery.of(context).size.width;
     // double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: SizedBox(
-        width: screenWidth > 430 ? 430 : screenWidth,
-        //height: screenHeight > 968 ? 968 : screenHeight,
-        child: Column(
-          children: [
-            //Score tracker
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Current Score'),
-                        Text(
-                          '$currentScore',
-                          style: TextStyle(
-                            fontSize: 30,
+      body: KeyboardListener(
+        focusNode: _focusNode,
+        autofocus: true,
+        onKeyEvent: (event) {
+          if (event.logicalKey == LogicalKeyboardKey.arrowDown &&
+              currentDirection != SnakeDirection.UP)
+            currentDirection = SnakeDirection.DOWN;
+          else if (event.logicalKey == LogicalKeyboardKey.arrowUp &&
+              currentDirection != SnakeDirection.DOWN)
+            currentDirection = SnakeDirection.UP;
+          else if (event.logicalKey == LogicalKeyboardKey.arrowLeft &&
+              currentDirection != SnakeDirection.RIGHT)
+            currentDirection = SnakeDirection.LEFT;
+          else if (event.logicalKey == LogicalKeyboardKey.arrowRight &&
+              currentDirection != SnakeDirection.LEFT)
+            currentDirection = SnakeDirection.RIGHT;
+        },
+        child: SizedBox(
+          width: screenWidth > 430 ? 430 : screenWidth,
+          //height: screenHeight > 968 ? 968 : screenHeight,
+          child: Column(
+            children: [
+              //Score tracker
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Current Score'),
+                          Text(
+                            '$currentScore',
+                            style: TextStyle(
+                              fontSize: 30,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Text(
-                          'Highscores',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text(
+                            'Highscores',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        isStarted
-                            ? Container()
-                            : FutureBuilder(
-                                future: getDocID(),
-                                builder: (BuildContext context,
-                                    AsyncSnapshot<dynamic> snapshot) {
-                                  return Expanded(
-                                    child: ListView.builder(
-                                      itemCount: docsID.length,
-                                      itemBuilder: (context, index) {
-                                        return HighScoreTile(docsID[index]);
-                                      },
-                                    ),
-                                  );
-                                },
-                              ),
-                      ],
+                          isStarted
+                              ? Container()
+                              : FutureBuilder(
+                                  future: getDocID(),
+                                  builder: (BuildContext context,
+                                      AsyncSnapshot<dynamic> snapshot) {
+                                    return Expanded(
+                                      child: ListView.builder(
+                                        itemCount: docsID.length,
+                                        itemBuilder: (context, index) {
+                                          return HighScoreTile(docsID[index]);
+                                        },
+                                      ),
+                                    );
+                                  },
+                                ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            //main board
-            Expanded(
-              flex: 3,
-              child: GestureDetector(
-                onVerticalDragUpdate: (details) {
-                  if (details.delta.dy > 0 &&
-                      currentDirection != SnakeDirection.UP)
-                    currentDirection = SnakeDirection.DOWN;
-                  else if (details.delta.dy < 0 &&
-                      currentDirection != SnakeDirection.DOWN)
-                    currentDirection = SnakeDirection.UP;
-                },
-                onHorizontalDragUpdate: (details) {
-                  if (details.delta.dx > 0 &&
-                      currentDirection != SnakeDirection.LEFT)
-                    currentDirection = SnakeDirection.RIGHT;
-                  else if (details.delta.dx < 0 &&
-                      currentDirection != SnakeDirection.RIGHT)
-                    currentDirection = SnakeDirection.LEFT;
-                },
-                child: GridView.builder(
-                  physics: NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  padding: EdgeInsets.zero,
-                  itemCount: gridPixels,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: gridColumns),
-                  itemBuilder: (build, index) {
-                    if (snakePosition.contains(index))
-                      return BoardTile(
-                        Colors.white,
-                      );
-                    else if (foodPosition == index)
-                      return BoardTile(Colors.green);
-                    else
-                      return BoardTile(Colors.grey[900]!);
+              //main board
+              Expanded(
+                flex: 3,
+                child: GestureDetector(
+                  onVerticalDragUpdate: (details) {
+                    if (details.delta.dy > 0 &&
+                        currentDirection != SnakeDirection.UP)
+                      currentDirection = SnakeDirection.DOWN;
+                    else if (details.delta.dy < 0 &&
+                        currentDirection != SnakeDirection.DOWN)
+                      currentDirection = SnakeDirection.UP;
                   },
+                  onHorizontalDragUpdate: (details) {
+                    if (details.delta.dx > 0 &&
+                        currentDirection != SnakeDirection.LEFT)
+                      currentDirection = SnakeDirection.RIGHT;
+                    else if (details.delta.dx < 0 &&
+                        currentDirection != SnakeDirection.RIGHT)
+                      currentDirection = SnakeDirection.LEFT;
+                  },
+                  child: GridView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    padding: EdgeInsets.zero,
+                    itemCount: gridPixels,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: gridColumns),
+                    itemBuilder: (build, index) {
+                      if (snakePosition.contains(index))
+                        return BoardTile(
+                          Colors.white,
+                        );
+                      else if (foodPosition == index)
+                        return BoardTile(Colors.green);
+                      else
+                        return BoardTile(Colors.grey[900]!);
+                    },
+                  ),
                 ),
               ),
-            ),
-            // play button and credits
-            Expanded(
-              child: Container(
-                child: Center(
-                  child: MaterialButton(
-                    onPressed: isStarted ? () {} : startGame,
-                    color: isStarted ? Colors.blueGrey : Colors.green,
-                    child: Text(
-                      'Start',
-                      style: TextStyle(fontSize: 20),
+              // play button and credits
+              Expanded(
+                child: Container(
+                  child: Center(
+                    child: MaterialButton(
+                      onPressed: isStarted ? () {} : startGame,
+                      color: isStarted ? Colors.blueGrey : Colors.green,
+                      child: Text(
+                        'Start',
+                        style: TextStyle(fontSize: 20),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
