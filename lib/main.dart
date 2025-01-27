8746d910a9eeb4d1916d32d544a8a689e3ce66b0
Muhammad-Moiz-Shafiq <m.moiz.cse@gmail.com>
2025-01-27 16:32:17 +0500

    fixed some bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:snake_game/main_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:snake_game/welcome_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyBSm6dLvd-WH1CIFoluZy4u435SaFdY624",
-          authDomain: "snake-game-flutter-35edf.firebaseapp.com",
-          projectId: "snake-game-flutter-35edf",
-          storageBucket: "snake-game-flutter-35edf.appspot.com",
-          messagingSenderId: "563393459754",
-          appId: "1:563393459754:web:a5ade293386d1b2b39b553",
-          measurementId: "G-ZD0HH81FSQ"));
+    options: FirebaseOptions(
+      apiKey: dotenv.env['API_KEY'] ?? '',
+      authDomain: dotenv.env['AUTH_DOMAIN'] ?? '',
+      projectId: dotenv.env['PROJECT_ID'] ?? '',
+      storageBucket: dotenv.env['STORAGE_BUCKET'] ?? '',
+      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID'] ?? '',
+      appId: dotenv.env['APP_ID'] ?? '',
+      measurementId: dotenv.env['MEASUREMENT_ID'] ?? '',
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -24,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: WelcomeScreen(),
+      home: const WelcomeScreen(),
     );
   }
 }
